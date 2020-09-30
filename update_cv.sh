f307eaeec122f4c0065b215304df8677f10147d8
Chang-Goo Kim <changgoo@princeton.edu>
2020-09-30 12:36:47 -0400

    Add cleanup script

--- update_cv.sh
+++ update_cv.sh
@@ -2,12 +2,18 @@
 
 cd CV/
 
+# update bibliography
 python get_pub.py
 python pub2tex.py
 
+# compile CV
 pdflatex CV.tex
 pdflatex CV.tex
 
+# cleanup
+rm -rf *
+git checkout .
+
+# move files
 cd ../
-
 cp CV/CV.pdf .
